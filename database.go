dbf5453b5d70f5ffe55e39839bbdb0fb9bf146b6
Benjamin Pape <benjaminpape@protonmail.com>
2025-02-06 20:15:00 +0000

    sitemap json generator

--- database.go
+++ database.go
@@ -34,37 +34,32 @@ func dbConnect(n int) error {
 	}
 
 	logMessage(5, "Creating tables")
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.configuration (item VARCHAR(16) NOT NULL, value VARCHAR(16) NOT NULL, favicon BYTEA NULL)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.configuration (item VARCHAR(16) NOT NULL PRIMARY KEY, value VARCHAR(16) NOT NULL, favicon BYTEA NULL)")
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.pages (page_uri VARCHAR(128) NOT NULL, page_title VARCHAR(128) NOT NULL DEFAULT 'New Page')")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.pages (page_uri VARCHAR(128) NOT NULL PRIMARY KEY, page_title VARCHAR(128) NOT NULL DEFAULT 'New Page')")
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.element (element_id VARCHAR(8) NOT NULL, element_name VARCHAR(128) NOT NULL DEFAULT 'New Element',  style_id VARCHAR(8) NOT NULL DEFAULT 'DEFAULTS', position_anchor VARCHAR(16) NULL, position_x INT NOT NULL DEFAULT '0', position_y INT NOT NULL DEFAULT '0', position_z INT NOT NULL DEFAULT '0', is_link SMALLINT NOT NULL DEFAULT '0' , link_url VARCHAR(128) NULL)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.elements (element_id VARCHAR(8) NOT NULL PRIMARY KEY, parent_type VARCHAR(8) NOT NULL DEFAULT 'page', parent_id VARCHAR(8) NULL, sequence_number INT NOT NULL DEFAULT '1', element_name VARCHAR(128) NOT NULL DEFAULT 'New Element',  style_id VARCHAR(8) NOT NULL DEFAULT 'DEFAULTS', pos_anchor VARCHAR(16) NULL, pos_x REAL NOT NULL DEFAULT '0', pos_y REAL NOT NULL DEFAULT '0', pos_z INT NOT NULL DEFAULT '0', width INT NOT NULL DEFAULT 10, height INT NOT NULL DEFAULT 10, is_link SMALLINT NOT NULL DEFAULT '0' , link_url VARCHAR(128) NULL, content TEXT NULL)")
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.element_parent (element_id VARCHAR(8) NOT NULL, parent_type VARCHAR(8) NOT NULL DEFAULT 'page', parent_id VARCHAR(8) NULL, sequence_number INT NOT NULL DEFAULT '1')")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.styles (style_id VARCHAR(8) NOT NULL PRIMARY KEY, style_name VARCHAR(128) NOT NULL DEFAULT 'new_style', background_type VARCHAR(8) NOT NULL DEFAULT 'color', background_data VARCHAR(128) NOT NULL DEFAULT '#cccccc', background_position VARCHAR(16) NOT NULL DEFAULT 'center', background_size VARCHAR(16) NOT NULL DEFAULT 'cover', font_family VARCHAR(32) NOT NULL DEFAULT 'sans-serif', font_size REAL NOT NULL DEFAULT '16', font_weight VARCHAR(16) NOT NULL DEFAULT 'normal', font_color VARCHAR(16) NOT NULL DEFAULT '#000000', margin REAL NOT NULL DEFAULT '0', padding REAL NOT NULL DEFAULT '0', text_align VARCHAR(16) NOT NULL DEFAULT 'left', border_width INT NOT NULL DEFAULT '0', border_style VARCHAR(16) NOT NULL DEFAULT 'solid', border_color VARCHAR(32) NOT NULL DEFAULT '#000000', custom_css TEXT NULL)")
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.styles (style_id VARCHAR(8) NOT NULL, style_name VARCHAR(128) NOT NULL DEFAULT 'new_style', background_type VARCHAR(8) NOT NULL DEFAULT 'color', background_data VARCHAR(128) NOT NULL DEFAULT '#cccccc', background_position VARCHAR(16) NOT NULL DEFAULT 'center', background_size VARCHAR(16) NOT NULL DEFAULT 'cover', font_family VARCHAR(32) NOT NULL DEFAULT 'sans-serif', font_size INT NOT NULL DEFAULT '16', font_weight VARCHAR(16) NOT NULL DEFAULT 'normal', font_color VARCHAR(16) NOT NULL DEFAULT '#000000', margin INT NOT NULL DEFAULT '0', padding INT NOT NULL DEFAULT '0', text_align VARCHAR(16) NOT NULL DEFAULT 'left', border_width INT NOT NULL DEFAULT '0', border_style VARCHAR(16) NOT NULL DEFAULT 'solid', border_color VARCHAR(32) NOT NULL DEFAULT '#000000', custom_css TEXT NULL)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.media (media_id VARCHAR(8) NOT NULL PRIMARY KEY, media_type VARCHAR(8) NOT NULL DEFAULT 'image', media_height INT NOT NULL, media_width INT NOT NULL, media_data BYTEA)")
 	if err != nil {
 		return err
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.media (media_id VARCHAR(8) NOT NULL, media_type VARCHAR(8) NOT NULL DEFAULT 'image', media_height INT NOT NULL, media_width INT NOT NULL, media_data BYTEA)")
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.editors (session_id VARCHAR(256) NOT NULL, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scrapbook_data.editors (session_id VARCHAR(256) NOT NULL PRIMARY KEY, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
 	if err != nil {
 		return err
 	}
--- http.go
+++ http.go
@@ -2,20 +2,66 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
 )
 
+type scrapbookSitemap struct {
+	Pages []scrapbookPage
+}
+
 type scrapbookPage struct {
+	Header   scrapbookPageHeader
+	Elements []scrapbookElement
+}
+
+type scrapbookElement struct {
+	ID        string
+	Name      string
+	StyleID   string
+	PosAnchor string
+	PosX      float32
+	PosY      float32
+	PosZ      int
+	Width     float32
+	Height    float32
+	IsLink    bool
+	LinkURL   string
+	Content   string
+	Children  []scrapbookElement
+}
+
+type scrapbookStyle struct {
+	ID                 string
+	Name               string
+	BackgroundType     string
+	BackgroundData     string
+	BackgroundPosition string
+	BackgroundSize     string
+	FontFamily         string
+	FontSize           float32
+	FontWeight         string
+	FontColor          string
+	Margin             float32
+	Padding            float32
+	TextAlign          string
+	BorderWidth        int
+	BorderStyle        string
+	BorderColor        string
+	CustomCSS          string
+}
+
+type scrapbookPageHeader struct {
 	Title string
 	URI   string
 }
 
 func httpHandler(response http.ResponseWriter, request *http.Request) {
 	var (
-		Title string
-		page  scrapbookPage
+		title string
+		page  scrapbookPageHeader
 	)
 
 	// Edit API
@@ -49,10 +95,47 @@ func httpHandler(response http.ResponseWriter, request *http.Request) {
 
 	}
 
+	// Serve sitemap
+	if request.URL.Path == "/sitemap.json" {
+		var (
+			pages []scrapbookPage
+		)
+
+		pageRows, err := db.Query("SELECT page_title, page_uri FROM scrapbook_data.pages")
+		if err != nil {
+			logMessage(2, err.Error())
+			return
+		}
+
+		for pageRows.Next() {
+			var title, uri string
+			pageRows.Scan(&title, &uri)
+
+			pages = append(pages, scrapbookPage{
+				scrapbookPageHeader{
+					title,
+					uri,
+				},
+				getNestedElements("page", uri),
+			})
+
+		}
+
+		jsonBytes, err := json.Marshal(pages)
+		if err != nil {
+			logMessage(2, err.Error())
+			return
+		}
+
+		fmt.Fprint(response, string(jsonBytes))
+
+		return
+	}
+
 	// Get page info from database
-	err := db.QueryRow("SELECT page_title FROM scrapbook_data.pages WHERE page_uri = $1", request.URL.Path).Scan(&Title)
+	err := db.QueryRow("SELECT page_title FROM scrapbook_data.pages WHERE page_uri = $1", request.URL.Path).Scan(&title)
 	if err == sql.ErrNoRows {
-		page = scrapbookPage{
+		page = scrapbookPageHeader{
 			"Page Not Found",
 			request.URL.Path,
 		}
@@ -60,14 +143,15 @@ func httpHandler(response http.ResponseWriter, request *http.Request) {
 	} else if err != sql.ErrNoRows && err != nil {
 		logMessage(2, err.Error())
 	} else {
-		page = scrapbookPage{
-			Title,
+		page = scrapbookPageHeader{
+			title,
 			request.URL.Path,
 		}
 	}
 
 	err = formTemplate.Execute(response, page)
 	if err != nil {
+		logMessage(1, err.Error())
 		fmt.Fprintf(response, "Error.")
 	}
 	logMessage(4, fmt.Sprintf("%s: %s", request.RemoteAddr, request.RequestURI))
@@ -82,4 +166,48 @@ func createEditorSession() (string, error) {
 		return newToken, nil
 	}
 	return "", err
+}
+
+func getNestedElements(parentType string, parentId string) []scrapbookElement {
+	elementRows, err := db.Query("SELECT element_id, element_name, style_id, pos_anchor, pos_x, pos_y, pos_z, width, height, is_link, link_url, content FROM scrapbook_data.elements WHERE parent_type = $1 AND parent_id = $2 ORDER BY sequence_number ASC", parentType, parentId)
+	if err != nil {
+		logMessage(2, err.Error())
+	}
+
+	var (
+		elements     []scrapbookElement = []scrapbookElement{}
+		element_id   string
+		element_name string
+		style_id     string
+		pos_anchor   string
+		pos_x        float32
+		pos_y        float32
+		pos_z        int
+		width        float32
+		height       float32
+		is_link      bool
+		link_url     string
+		content      string
+	)
+
+	for elementRows.Next() {
+		elementRows.Scan(&element_id, &element_name, &style_id, &pos_anchor, &pos_x, &pos_y, &pos_z, &width, &height, &is_link, &link_url, &content)
+		elements = append(elements, scrapbookElement{
+			element_id,
+			element_name,
+			style_id,
+			pos_anchor,
+			pos_x,
+			pos_y,
+			pos_z,
+			width,
+			height,
+			is_link,
+			link_url,
+			content,
+			getNestedElements("element", element_id),
+		})
+	}
+	return elements
+
 }
